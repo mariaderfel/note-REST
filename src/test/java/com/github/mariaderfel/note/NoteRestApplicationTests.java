4a78eb5c6ed9be6f6c8ca9c6ef18a1560c60959a
mariaderfel <mariaderfel@gmail.com>
2021-05-06 10:27:13 +0200

    create project

---
+++ src/test/java/com/github/mariaderfel/note/NoteRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mariaderfel.note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
